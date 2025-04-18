4d6b5912a7a743a7e3f1aeea8af2fed4f5700717
Orjan Ameye <orjan.ameye@hotmail.com>
2025-04-18 18:47:32 +0200

    fix: turn of JET on julia prerelease

--- test/code_quality.jl
+++ test/code_quality.jl
@@ -1,6 +1,8 @@
-@testset "Code linting" begin
-    using JET
-    JET.test_package(HarmonicBalance; target_defined_modules=true)
+if VERSION < v"1.12.0-beta"
+    @testset "Code linting" begin
+        using JET
+        JET.test_package(HarmonicBalance; target_defined_modules=true)
+    end
 end
 
 @testset "Code quality" begin
@@ -11,19 +13,10 @@ end
     @test check_no_stale_explicit_imports(HarmonicBalance) == nothing
     @test check_all_explicit_imports_via_owners(HarmonicBalance) == nothing
     Aqua.test_ambiguities([HarmonicBalance])
-    Aqua.test_all(
-        HarmonicBalance;
-        # deps_compat=(
-        #     ignore=ignore_deps,
-        #     check_extras=(ignore=ignore_deps,),
-        #     check_weakdeps=(ignore=ignore_deps,),
-        # ),
-        # ambiguities=false,
-    )
+    Aqua.test_all(HarmonicBalance;)
     for mod in [ModelingToolkitExt]
         @test check_no_stale_explicit_imports(mod) == nothing
         @test check_all_explicit_imports_via_owners(mod) == nothing
-        # Aqua.test_ambiguities(mod)
         Aqua.test_all(
             mod;
             deps_compat=false,
